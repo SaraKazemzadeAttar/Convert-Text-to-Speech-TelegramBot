15f92740d34391f9fbc89c2b4c9203b6d5407165
SaraKazemzadeAttar <Sara.kazemzade.atar2004@gmail.com>
2025-02-19 19:25:27 +0330

    import logging module

--- core/main.py
+++ core/main.py
@@ -1,11 +1,11 @@
 import telebot
 import os
+import logging
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 API_TOKEN = os.environ.get('API_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
-
 
 
 
